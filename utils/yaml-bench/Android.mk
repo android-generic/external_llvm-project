339edd13dd64e72756a7bec20c0333c45f39d3ea
HMTheBoy154 <buingoc67@gmail.com>
2020-03-13 10:41:15 +0700

    Revert "DO NOT MERGE: android: translate Android.mk build rules for libLLVM80"
    This reverts commit ee641c7203007c7ecc0306d489e37d1f00e521bb.

--- utils/yaml-bench/Android.mk
+++ utils/yaml-bench/Android.mk
@@ -1,13 +1,13 @@
 LOCAL_PATH := $(call my-dir)
 
-LLVM80_ROOT_PATH := $(LOCAL_PATH)/../..
+LLVM70_ROOT_PATH := $(LOCAL_PATH)/../..
 
 
 yaml_bench_SRC_FILES := \
   YAMLBench.cpp
 
 yaml_bench_STATIC_LIBRARIES := \
-  libLLVM80Support \
+  libLLVM70Support \
 
 # yaml-bench executable for the host
 # =====================================================
@@ -23,6 +23,6 @@ LOCAL_SRC_FILES := $(yaml_bench_SRC_FILES)
 
 LOCAL_STATIC_LIBRARIES := $(yaml_bench_STATIC_LIBRARIES)
 
-include $(LLVM80_ROOT_PATH)/llvm.mk
-include $(LLVM80_HOST_BUILD_MK)
+include $(LLVM70_ROOT_PATH)/llvm.mk
+include $(LLVM70_HOST_BUILD_MK)
 include $(BUILD_HOST_EXECUTABLE)
