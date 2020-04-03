maurossi/llvm

android: fix Target/ARM build
Changelog:

updated list of *.cpp sources in ARM Android.mk makefiles
added lib/Target/ARM/Utils/Android.mk to build libLLVMARMUtils module
shared_llvm.mk: added libLLVMARMUtils to arm static libraries
main Android.mk: lib/Target/ARM/Utils path added
llvm-tblgen-rules.mk: new rule for ARMGenSystemRegister.inc aligned to AMR Cmakelist.txt
ARM modules Android.mk: added ARMGenSystemRegister.inc generated file where necessary
ARM modules Android.mk: added *GEN_ATTRIBUTES_MK, *GEN_INTRINSICS_MK rules where necessary

lib/Target/ARM/Utils/Android.mk
@@ -1,54 +1,51 @@
 LOCAL_PATH := $(call my-dir)
 
-arm_disassembler_TBLGEN_TABLES60 := \
-  ARMGenDisassemblerTables.inc \
+arm_utils_TBLGEN_TABLES60 := \
+  ARMGenRegisterInfo.inc \
   ARMGenInstrInfo.inc \
   ARMGenSubtargetInfo.inc \
-  ARMGenRegisterInfo.inc \
+  ARMGenSystemOperands.inc \
   ARMGenSystemRegister.inc
 
-arm_disassembler_SRC_FILES := \
-  ARMDisassembler.cpp
+arm_utils_SRC_FILES := \
+  ARMBaseInfo.cpp
 
 # For the device
 # =====================================================
 ifneq (true,$(DISABLE_LLVM_DEVICE_BUILDS))
 include $(CLEAR_VARS)
-include $(CLEAR_TBLGEN_VARS60)
 
-TBLGEN_TABLES60 := $(arm_disassembler_TBLGEN_TABLES60)
-
-TBLGEN_TD_DIR60 := $(LOCAL_PATH)/..
-
-LOCAL_SRC_FILES := $(arm_disassembler_SRC_FILES)
+LOCAL_SRC_FILES := $(arm_utils_SRC_FILES)
 
 LOCAL_C_INCLUDES += $(LOCAL_PATH)/..
 
-LOCAL_MODULE:= libLLVM60ARMDisassembler
+LOCAL_MODULE:= libLLVM60ARMUtils
+
+TBLGEN_TD_DIR60 := $(LOCAL_PATH)/..
+TBLGEN_TABLES60 := $(arm_utils_TBLGEN_TABLES60)
 
 include $(LLVM60_DEVICE_BUILD_MK)
 include $(LLVM60_TBLGEN_RULES_MK)
+include $(LLVM60_GEN_INTRINSICS_MK)
 include $(BUILD_STATIC_LIBRARY)
 endif
 
 # For the host
 # =====================================================
 include $(CLEAR_VARS)
-include $(CLEAR_TBLGEN_VARS60)
 
-TBLGEN_TABLES60 := $(arm_disassembler_TBLGEN_TABLES60)
-
-TBLGEN_TD_DIR60 := $(LOCAL_PATH)/..
-
-LOCAL_SRC_FILES := $(arm_disassembler_SRC_FILES)
+LOCAL_SRC_FILES := $(arm_utils_SRC_FILES)
 
 LOCAL_C_INCLUDES += $(LOCAL_PATH)/..
 
-LOCAL_MODULE:= libLLVM60ARMDisassembler
+LOCAL_MODULE:= libLLVM60ARMUtils
 
 LOCAL_MODULE_HOST_OS := darwin linux windows
 
+TBLGEN_TD_DIR60 := $(LOCAL_PATH)/..
+TBLGEN_TABLES60 := $(arm_utils_TBLGEN_TABLES60)
+
 include $(LLVM60_HOST_BUILD_MK)
 include $(LLVM60_TBLGEN_RULES_MK)
+include $(LLVM60_GEN_INTRINSICS_MK)
 include $(BUILD_HOST_STATIC_LIBRARY)
-

llvm-tblgen-rules.mk
@@ -230,6 +230,13 @@ $(generated_sources)/ARMGenDecoderTables.inc: $(tblgen_source_dir)/ARM.td \
 	$(call transform-td-to-out60,arm-decoder)
 endif
 
+ifneq ($(findstring ARMGenSystemRegister.inc,$(tblgen_gen_tables)),)
+$(generated_sources)/ARMGenSystemRegister.inc: TBLGEN_LOCAL_MODULE := $(LOCAL_MODULE)
+$(generated_sources)/ARMGenSystemRegister.inc: $(tblgen_source_dir)/ARM.td \
+                                          $(tblgen_td_deps) $(LLVM60_TBLGEN)
+	$(call transform-td-to-out60,searchable-tables)
+endif
+
 ifneq ($(findstring Options.inc,$(tblgen_gen_tables)),)
 $(generated_sources)/Options.inc: TBLGEN_LOCAL_MODULE := $(LOCAL_MODULE)
 $(generated_sources)/Options.inc: $(tblgen_source_dir)/Options.td \

android-generic/external_llvm-project

lib/Target/ARM/Utils/Android.mk
@@ -1,54 +1,51 @@
 LOCAL_PATH := $(call my-dir)
 
-arm_disassembler_TBLGEN_TABLES70 := \
-  ARMGenDisassemblerTables.inc \
+arm_utils_TBLGEN_TABLES70 := \
+  ARMGenRegisterInfo.inc \
   ARMGenInstrInfo.inc \
   ARMGenSubtargetInfo.inc \
-  ARMGenRegisterInfo.inc \
+  ARMGenSystemOperands.inc \
   ARMGenSystemRegister.inc
 
-arm_disassembler_SRC_FILES := \
-  ARMDisassembler.cpp
+arm_utils_SRC_FILES := \
+  ARMBaseInfo.cpp
 
 # For the device
 # =====================================================
 ifneq (true,$(DISABLE_LLVM_DEVICE_BUILDS))
 include $(CLEAR_VARS)
-include $(CLEAR_TBLGEN_VARS70)
 
-TBLGEN_TABLES70 := $(arm_disassembler_TBLGEN_TABLES70)
-
-TBLGEN_TD_DIR70 := $(LOCAL_PATH)/..
-
-LOCAL_SRC_FILES := $(arm_disassembler_SRC_FILES)
+LOCAL_SRC_FILES := $(arm_utils_SRC_FILES)
 
 LOCAL_C_INCLUDES += $(LOCAL_PATH)/..
 
-LOCAL_MODULE:= libLLVM70ARMDisassembler
+LOCAL_MODULE:= libLLVM70ARMUtils
+
+TBLGEN_TD_DIR70 := $(LOCAL_PATH)/..
+TBLGEN_TABLES70 := $(arm_utils_TBLGEN_TABLES70)
 
 include $(LLVM70_DEVICE_BUILD_MK)
 include $(LLVM70_TBLGEN_RULES_MK)
+include $(LLVM70_GEN_INTRINSICS_MK)
 include $(BUILD_STATIC_LIBRARY)
 endif
 
 # For the host
 # =====================================================
 include $(CLEAR_VARS)
-include $(CLEAR_TBLGEN_VARS70)
 
-TBLGEN_TABLES70 := $(arm_disassembler_TBLGEN_TABLES70)
-
-TBLGEN_TD_DIR70 := $(LOCAL_PATH)/..
-
-LOCAL_SRC_FILES := $(arm_disassembler_SRC_FILES)
+LOCAL_SRC_FILES := $(arm_utils_SRC_FILES)
 
 LOCAL_C_INCLUDES += $(LOCAL_PATH)/..
 
-LOCAL_MODULE:= libLLVM70ARMDisassembler
+LOCAL_MODULE:= libLLVM70ARMUtils
 
 LOCAL_MODULE_HOST_OS := darwin linux windows
 
+TBLGEN_TD_DIR70 := $(LOCAL_PATH)/..
+TBLGEN_TABLES70 := $(arm_utils_TBLGEN_TABLES70)
+
 include $(LLVM70_HOST_BUILD_MK)
 include $(LLVM70_TBLGEN_RULES_MK)
+include $(LLVM70_GEN_INTRINSICS_MK)
 include $(BUILD_HOST_STATIC_LIBRARY)
-

llvm-tblgen-rules.mk
@@ -230,6 +230,13 @@ $(generated_sources)/ARMGenDecoderTables.inc: $(tblgen_source_dir)/ARM.td \
 	$(call transform-td-to-out70,arm-decoder)
 endif
 
+ifneq ($(findstring ARMGenSystemRegister.inc,$(tblgen_gen_tables)),)
+$(generated_sources)/ARMGenSystemRegister.inc: TBLGEN_LOCAL_MODULE := $(LOCAL_MODULE)
+$(generated_sources)/ARMGenSystemRegister.inc: $(tblgen_source_dir)/ARM.td \
+                                          $(tblgen_td_deps) $(LLVM70_TBLGEN)
+	$(call transform-td-to-out70,searchable-tables)
+endif
+
 ifneq ($(findstring Options.inc,$(tblgen_gen_tables)),)
 $(generated_sources)/Options.inc: TBLGEN_LOCAL_MODULE := $(LOCAL_MODULE)
 $(generated_sources)/Options.inc: $(tblgen_source_dir)/Options.td \